91a213bf195d4f1109c819de39ce8e1ce0891565
jualos <juanpablo.410@hotmail.com.ar>
2023-11-11 16:31:16 -0300

    solucionada

--- src/app.js
+++ src/app.js
@@ -8,4 +8,11 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/api/cart', cart)
+app.use('/api/product', product)
+
+app.listen(port, () => {
+    console.log(`Servicio activo en puerto ${port}`)
+})
+
 
